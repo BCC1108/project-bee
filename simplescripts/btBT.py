d09fc5305e532f63c0371625ae332023276eeee8
BCC1108 <cbn1108@foxmail.com>
2026-03-06 03:36:21 +0800

    对hq6交易系统B进行逐事件回测

--- simplescripts/btBT.py
+++ simplescripts/btBT.py
@@ -1,14 +1,19 @@
 #backtrader库
+import sys
+import os
+from threading import main_thread
+
+from numpy import long
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__))) 
+from core.commonFunctions import make_df 
 import backtrader as bt
 import backtrader.indicators as btind
-from core.commonFunctions import make_df
-
-#绘图库
 
 
 #别的库
 import pandas as pd
-from tqdm import tqdm , trange
+from tqdm import tqdm 
 import json
 import time
 
@@ -16,58 +21,89 @@ import time
 
 #===全局变量====
 resamplenum = 60 * 60          #K线重聚和时间，秒计算
+resamplenum2 = 60 * 15         #K线重聚和时间，秒计算
 tradeMin = 0.01*0.001          #swapCcy * swapMin
-datafilename = 'soldata'
+datafilename = 'soldata.parquet'
 
 #===此地不需要修改===
+class SuperTrend(bt.Indicator):
+    lines = ('supertrend', 'direction')
+    params = (('period', 10), ('multiplier', 3),)
 
-def got_df(datafilename):
-    with open(datafilename) as databook:
-        data = json.load(databook)
-        
-    df = pd.DataFrame(data, columns=[
-            "ts", "open", "high", "low", "close", "vol", "volCcy", "volCcyQuote", "confirm"
-        ])
-    #df = df.iloc[::-1].reset_index(drop=True)  # oldest → newest
+    def __init__(self):
+        self.atr = bt.indicators.ATR(period=self.p.period)            #type: ignore
+        self.hl2 = (self.data.high + self.data.low) / 2.0
+        self.upper = self.hl2 + self.p.multiplier * self.atr
+        self.lower = self.hl2 - self.p.multiplier * self.atr
 
-    df['ts'] = pd.to_numeric(df['ts'])
-    df = df.sort_values('ts').reset_index(drop=True)
-    
-    df['datetime'] = pd.to_datetime(df['ts'], unit='ms')
-    for col in ['open', 'high', 'low', 'close', 'vol']:
-        df[col] = pd.to_numeric(df[col])
-    
-    df = df[['datetime', 'open', 'high', 'low', 'close', 'vol']]
-    #此处应有一行转换时区
-    df.rename(columns={'vol': 'volume'}, inplace=True)
-    df.set_index('datetime', inplace=True)
-    #df = df[::5]       #调试用
-    
-    
-    return df
+        # 初始化状态（第一根K线）
+        self.initialized = False
+    def next(self):
+        if not self.initialized:
+            self.lines.supertrend[0] = self.lower[0]                                   #type: ignore
+            self.lines.direction[0] = 1  # 1=多, -1=空                                 #type: ignore
+            self.initialized = True
+            return
+            
+        prev_st = self.lines.supertrend[-1]                                  #type: ignore
+        prev_dir = self.lines.direction[-1]                                  #type: ignore
+        close = self.data.close[0]
+        upper = self.upper[0]
+        lower = self.lower[0]
+
+        if prev_dir == 1:  # 当前是多头
+            if close < upper:
+                # 翻转为空头
+                self.lines.supertrend[0] = upper                             #type: ignore
+                self.lines.direction[0] = -1                                 #type: ignore
+            else:
+                # 维持多头，st 只能上移
+                self.lines.supertrend[0] = max(lower, prev_st)               #type: ignore
+        else:  # 当前是空头 
+            if close > lower:
+                # 翻转为多头
+                self.lines.supertrend[0] = lower                             #type: ignore
+                self.lines.direction[0] = 1                                  #type: ignore
+            else:
+                # 维持空头，st 只能下移
+                self.lines.supertrend[0] = min(upper, prev_st)               #type: ignore
 
 # Create a Stratey
 class hqStrategy(bt.Strategy):
     params = (
-        ('testperiod', 20 ),
         ('take_profit_rate' , 0.08),
         ('stop_loss_rate' , 0.08)
     )
 
-    def pr(self, prtxt , dt=None):                             # dt = None  如果不传入参数，就自动使用当前k线的日期
-        dt = dt or self.datas[0].datetime.datetime(0)          #self.data.datetime[0] 是完整的“日期+时间”（精确到秒/毫秒），而 self.data.datetime.date(0) 只取“日期部分”（年-月-日），去掉时分秒。
-        time_str = dt.strftime("%Y-%m-%d %H:%M:%S")
-        tqdm.write('%s, %s' % (time_str, prtxt))
-        
-
+    
     def __init__(self):
-        self.boll = btind.BollingerBands(self.data1, period = self.p.testperiod)    #type: ignore
-        self.body_high = bt.Max(self.data1.open, self.data1.close)   # 柱体上沿
-        self.body_low  = bt.Min(self.data1.open, self.data1.close)   # 柱体下沿
+        #15min指标
+        self.boll_15min = btind.BollingerBands(self.data1, period =20 , devfactor = 2)                 #type: ignore
+        self.rsi_15min = btind.RSI(self.data1, period = 14)                                            #type: ignore
+        self.atr_15min = btind.ATR(self.data1, period = 14)                                            #type: ignore
+        self.volRation_15min = self.data1.volume / btind.SMA(self.data1.volume, period = 20)           #type: ignore
+        self.superTrend_15min = SuperTrend(self.data1, period = 7, multiplier = 2.0)                    #type: ignore
+        
+        #1h指标
+        self.boll_60min= btind.BollingerBands(self.data2, period =20 , devfactor = 2)                  #type: ignore
+        self.rsi_60min = bt.ind.RSI(self.data2, period=14)                                             #type: ignore
+        self.vol_ratio_60 = self.data2.volume / bt.ind.SMA(self.data2.volume, period=20)               #type: ignore
+        # RAVI = (SMA(short) - SMA(long)) / SMA(long) * 100
+        sma_short = bt.ind.SMA(self.data2.close, period=7)                                             #type: ignore
+        sma_long = bt.ind.SMA(self.data2.close, period=65)                                             #type: ignore
+        self.ravi_60 = (sma_short - sma_long) / sma_long * 100
+        self.st_60 = SuperTrend(self.data2, period= 10 , multiplier=2.5)                               #type: ignore
+        
+        #辅助变量
         self.order = None 
         self.win_trades = 0
         self.total_trades = 0
         self.data1_len = 0    
+    
+    def pr(self, prtxt , dt=None):                             # dt = None  如果不传入参数，就自动使用当前k线的日期
+        dt = dt or self.datas[0].datetime.datetime(0)          #self.data.datetime[0] 是完整的“日期+时间”（精确到秒/毫秒），而 self.data.datetime.date(0) 只取“日期部分”（年-月-日），去掉时分秒。
+        time_str = dt.strftime("%Y-%m-%d %H:%M:%S")
+        tqdm.write('%s, %s' % (time_str, prtxt))
 
     def notify_order(self, order):
         if order.status in [order.Submitted , order.Accepted]:
@@ -105,23 +141,86 @@ class hqStrategy(bt.Strategy):
         
         if self.order:
             return
-
-        buysignal = (
-            self.data1.low[-1] < self.boll.bot[-1] and           # 第一根下破下轨
-            self.data1.close[0] > self.data1.open[0] and         # 第二根是绿线（阳线）
-            self.data1.volume[0] < self.data1.volume[-1]         # 第二根缩量
-        )
-
-        sellsignal = (
-            self.data1.high[-1] > self.boll.top[-1] and          # 第一根上穿上轨
-            self.data1.close[0] < self.data1.open[0] and         # 第二根是红线（阴线）
-            self.data1.volume[0] < self.data1.volume[-1]         # 第二根缩量
-        )
         
-        cLongsig = (self.data1.close[-1] >= self.boll.mid[-1]) and (self.data1.close[0] < self.boll.mid[0])      # 多头：收盘回穿中轨
-        cShortsig = (self.data1.close[-1] <= self.boll.mid[-1])  and (self.data1.close[0] > self.boll.mid[0])    # 空头：收盘回穿中轨
+        #市场动态计算
+        main_thread = 0
+        if self.st_60.lines.direction[-1] == 1 and self.ravi_60[0] > 1.0:                   # 60分钟周期多头趋势              #type: ignore
+            main_thread = 1 
+        elif self.st_60.lines.direction[-1] == -1 and self.ravi_60[0] < -1.0:               # 60分钟周期空头趋势              #type: ignore
+            main_thread = -1
         
+        sub_thread = self.superTrend_15min.lines.direction[0]                               # 15分钟周期趋势                  #type: ignore
+        momentum = 1 if self.rsi_60min[0] > 50 else (-1 if self.rsi_60min[0] < 50 else 0 )  # 15分钟周期动量
+        
+        final_score = 0.6 * main_thread + 0.3 * sub_thread + 0.1 * momentum
+        
+        #市场分类
+        up_trend = final_score > 0.5           #多头
+        down_trend = final_score < -0.5         #空头
+        upNdown = abs(final_score) <= 0.5        #震荡
+        
+        buysignal = False
+        sellsignal = False
+        
+        
+        #入场信号
+        if upNdown:
+            buysignal = (
+                self.data1.low[-1] < self.boll_15min.lines.bot[-1] and      #下破下轨    #type: ignore
+                self.data1.close[0] > self.data1.open[0] and                #绿线
+                self.volRation_15min[0] < 0.8                               #缩量
+            )
+                
+
+            sellsignal = (
+                self.data1.high[-1] > self.boll_15min.lines.top[-1] and     #上破上轨    #type: ignore
+                self.data1.close[0] < self.data1.open[0] and                #红线
+                self.volRation_15min[0] < 0.8                               #缩量     
+                )
+        
+        elif up_trend:
+            buysignal = (
+                self.superTrend_15min.lines.direction[0] == 1 and                           # 15m 仍为多头                     #type: ignore
+                self.data1.close[-1] > self.superTrend_15min.lines.supertrend[-1] and       # 前一根在ST上方                   #type: ignore
+                self.data1.low[0] <= self.superTrend_15min.lines.supertrend[0] * 1.005 and  # 当前低点接近ST（允许0.5%滑点）    #type: ignore
+                self.data1.close[0] > self.data1.open[0] and                                # 收阳确认
+                self.rsi_15min[0] > 45                                                      # RSI未进入超卖（>45）
+            )
+        
+        elif down_trend:
+            sellsignal = (
+                self.superTrend_15min.lines.direction[0] == -1 and                          # 15m 仍为空头                    #type: ignore
+                self.data1.close[-1] < self.superTrend_15min.lines.supertrend[-1] and       # 前一根在ST下方                  #type: ignore
+                self.data1.high[0] >= self.superTrend_15min.lines.supertrend[0] * 0.995 and # 当前高点接近ST                  #type: ignore
+                self.data1.close[0] < self.data1.open[0] and                                # 收阴确认
+                self.rsi_15min[0] < 55                                                      # RSI未进入超买（<55）
+            )
+        
+        
+        #出场信号
+        cLongsig = False
+        cShortsig = False
         pos = self.getposition(self.data)
+        
+        if pos.size > 0:
+            if upNdown:
+                cLongsig = (self.data1.close[-1] < self.boll_15min.lines.mid[-1]) and (self.data1.close[0] > self.boll_15min.lines.mid[0])          #type: ignore
+                
+            else:
+                cLongsig = (
+                    self.superTrend_15min.lines.direction[0] == -1 or                                                                         #type: ignore
+                    self.data1.close[0] < self.superTrend_15min.lines.supertrend[0]                                                           #type: ignore
+                )
+        elif pos.size < 0:
+            if upNdown:
+                cShortsig = (self.data1.close[-1] > self.boll_15min.lines.mid[-1]) and (self.data1.close[0] < self.boll_15min.lines.mid[0])          #type: ignore
+                
+            else:
+                cShortsig = (
+                    self.superTrend_15min.lines.direction[0] == 1 or                                                                         #type: ignore
+                    self.data1.close[0] > self.superTrend_15min.lines.supertrend[0]                                                           #type: ignore
+                )
+            
         
         if len(self.data):
             if pos.size != 0:
@@ -193,13 +292,14 @@ if __name__ == '__main__':
     
     cerebro.addstrategy(hqStrategy)
    
-    df = got_df(datafilename)
+    df = make_df(datafilename)
+    
     totalbar = len(df)
     pbar = tqdm(total=totalbar , desc = '回测进度' , unit = "klines" , position = 0 , leave = True , bar_format='{desc}: {percentage:.2f}%|{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}]')
 
     data = bt.feeds.PandasData(                                                 
         dataname=df,                                                   # type: ignore
-        datetime=None,                                                 # type: ignore
+        datetime=None,                                                 # 时间是df的index   # type: ignore
         open=0,                                                        # type: ignore
         high=1,                                                        # type: ignore
         low=2,                                                         # type: ignore
@@ -216,6 +316,13 @@ if __name__ == '__main__':
         data,
         timeframe = bt.TimeFrame.Seconds,                              #type: ignore
         compression = resamplenum,
+        name = 'data2'
+    )
+    
+    cerebro.resampledata(
+        data,
+        timeframe = bt.TimeFrame.Seconds,                              #type: ignore
+        compression = resamplenum2,
         name = 'data1'
     )
     
@@ -245,8 +352,6 @@ if __name__ == '__main__':
     tqdm.write('出场身价: %.2f '% cerebro.broker.getvalue() )
     t1 = time.time()
     tqdm.write(f">>> 回测完成，耗时: {t1 - t0:.2f} 秒")
-    
-    strat = result[0]
 
     cerebro.plot()
     
